e01e61644de73beba9c06d2362ae171d1273b349
Nisha2018 <38083071+Nisha2018@users.noreply.github.com>
2020-03-27 20:04:58 +0530

    initial

---
+++ js/kregzo.js
@@ -0,0 +1,13 @@
+let next = document.querySelector('.right');
+
+next.addEventListener("click", () => {
+	document.querySelector('.img-container img.creator').classList.add('animate');
+	document.querySelector('.img-container img.box').classList.add('animate');
+});
+
+
+
+
+	
+	
+
