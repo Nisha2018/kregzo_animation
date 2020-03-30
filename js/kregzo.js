2b7ceba2a5769bb4204f9624e9af77d882c817ee
Nisha2018 <38083071+Nisha2018@users.noreply.github.com>
2020-03-30 13:16:15 +0530

    changes

--- js/kregzo.js
+++ js/kregzo.js
@@ -4,22 +4,14 @@ let next1 = document.querySelector('.right1');
 let prev1 = document.querySelector('.left1');
 
 next.addEventListener("click", () => {
-	document.querySelector('.img-container').classList.add('creator');
-	document.querySelector('.content-right .text1').classList.add('hide');
-	document.querySelector('.content-right .text2').classList.add('show');
+	document.querySelector('#home').classList.add('creator-view');
 });
 
 prev.addEventListener("click",() => {
-	document.querySelector('.img-container').classList.remove('creator');
-	document.querySelector('.content-right .text1').classList.remove('hide');
-	document.querySelector('.content-right .text2').classList.remove('show');
+	document.querySelector('#home').classList.remove('creator-view');
 });
 
-// next1.addEventListener("click", () => {
-// 	document.querySelector('.img-container').classList.add('mentor-view');
-// 	document.querySelector('.content-right .text2').classList.add('hide');
-// 	document.querySelector('.content-right .text3').classList.add('show');
-// });
+
 
 
 
