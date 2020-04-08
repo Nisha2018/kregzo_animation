1d0aa807a66a32151ed8ec410fcc039ea9653213
Nisha2018 <38083071+Nisha2018@users.noreply.github.com>
2020-04-08 18:13:53 +0530

    changes

--- js/kregzo.js
+++ js/kregzo.js
@@ -11,9 +11,9 @@ let creator1 = document.querySelector(".creator1");
 let gear1 = document.querySelector(".gear1");
 let gear2 = document.querySelector(".gear2");
 let gear3 = document.querySelector(".gear3");
-let gear4 =document.querySelector(".gear4");
-let box =document.querySelector(".box");
-let panel =document.querySelector(".panel");
+let gear4 = document.querySelector(".gear4");
+let box = document.querySelector(".box");
+let panel = document.querySelector(".panel");
 
 var counter = 1;
 
@@ -22,59 +22,44 @@ next.classList.add("button-hover2");
 prev.classList.add("button-hover");
 
 next.addEventListener("click", () => {
-    if(counter === 1) {
-        home.classList.add("creator-view");
+    if (counter === 1) {
+        home.classList.add("mentor-view");
         counter++;
         prev.disabled = false;
         prev.classList.remove("button-hover");
         prev.classList.add("button-hover2");
-    } else if(counter === 2) {
-        home.classList.add("mentor-view");
-        counter++;
-
-    } else if(counter === 3) {
+    } else if (counter === 2) {
         home.classList.add("investor-view");
         counter++;
-        // next.disabled = true;
-        // next.classList.remove("button-hover2");
-        // next.classList.add("button-hover");
-        
         prev.disabled = false;
         prev.classList.add("button-hover2");
- 
-    } else if(counter===4){
-        home.classList.remove("investor-view","mentor-view","creator-view");
+
+    } else if (counter === 3) {
+        home.classList.remove("investor-view", "mentor-view");
         counter = 1;
         prev.disabled = true;
         prev.classList.remove("button-hover2");
         prev.classList.add("button-hover");
     }
 
-}); 
+});
 
 
 prev.addEventListener("click", () => {
 
-    if (counter === 4) {
+    if (counter === 3) {
         home.classList.remove("investor-view");
         counter--;
         // next.disabled = false;
         // next.classList.remove("button-hover");
         // next.classList.add("button-hover2");
-    } else if (counter === 3) {
-        home.classList.remove("mentor-view");
-        counter--;
     } else if (counter === 2) {
-        home.classList.remove("creator-view");
+        home.classList.remove("mentor-view");
         counter--;
         prev.disabled = true;
         prev.classList.remove("button-hover2");
         prev.classList.add("button-hover");
-        
-        // next.disabled = false;
-        // next.classList.remove("button-hover");
-        // next.classList.add("button-hover2");
-    } 
+    }
 
 });
 
@@ -86,44 +71,79 @@ window.addEventListener('scroll', () => {
 
     filament.style.left = value * 1 + 'px';
     base.style.left = value * 1 + 'px';
-    gear1.style.top = value + 500  + 'px';
+    gear1.style.top = value + 500 + 'px';
+    gear2.style.top = value + 200 + 'px';
+    gear3.style.top = value + 500 + 'px';
+    gear4.style.top = value + 500 + 'px';
+    box.style.bottom = value * 1 + 200 + 'px';
 
-    if(window.pageYOffset <= 20){
+    if (window.pageYOffset == 0) {
+        box.style.visibility = "hidden";
+        gear1.style.visibility = "hidden";
+        gear2.style.visibility = "hidden";
+        gear3.style.visibility = "hidden";
+        gear4.style.visibility = "hidden";
+        box.style.display="block";
+    } else {
+        box.style.visibility = "visible";
+        gear1.style.visibility = "visible";
+        gear2.style.visibility = "visible";
+        gear3.style.visibility = "visible";
+        gear4.style.visibility = "visible";
+    }
+
+    if (window.pageYOffset <= 20) {
         imgContainer.style.position = "relative";
         content.style.position = "relative";
-    }
-    else{
+    } else {
         imgContainer.style.position = "fixed";
         // content.style.position = "fixed";
     }
 
-    if(window.pageYOffset >= 150){
+     if (window.pageYOffset <= 122){
+        home.classList.remove("mentor-view");
+        home.classList.remove("investor-view");
+     }
+
+    if (window.pageYOffset >= 150) {
         filament.style.left = 36 + '%';
         base.style.left = 39.5 + '%';
-        gear1.setAttribute("style","top:10%;left:20%;");
+        gear1.setAttribute("style", "top:10%;left:20%;");
+        gear2.setAttribute("style", "top:34%;left:18%;");
+        gear3.setAttribute("style", "top:40%;left:56%;");
+        gear4.setAttribute("style", "top:22%;left:62%;");
+        box.style.bottom = 140 + '%';
 
         home.classList.add("creator-view");
         temp.classList.add("show");
-        content.style.top = 246+"px";
+        content.style.top = 246 + "px";
         mouse.style.display = "none";
         prev.style.display = "block";
         next.style.display = "block";
     } else {
         home.classList.remove("creator-view");
-        content.style.top = 0+"px";
+        content.style.top = 0 + "px";
         temp.classList.remove("show");
         mouse.style.display = "block";
         prev.style.display = "none";
         next.style.display = "none";
     }
 
-    if(window.pageYOffset >= 400) {
+    if (window.pageYOffset >= 400) {
         imgContainer.style.position = "relative";
         content.style.position = "relative";
         prev.style.display = "none";
         next.style.display = "none";
+        box.style.display="none";
     }
-    
-    
+
+
 });
 
+
+// scroll down
+
+mouse.addEventListener("click",()=>{
+    // window.scrollBy(0,250);
+    window.scrollTo({ top: 250, behavior: 'smooth' })
+})
