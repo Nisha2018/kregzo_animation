228063640325c2a7ebcdf7e8c684b4e228aabbeb
Nisha2018 <38083071+Nisha2018@users.noreply.github.com>
2020-04-28 17:16:12 +0530

    changes

--- js/kregzo.js
+++ js/kregzo.js
@@ -104,7 +104,7 @@ tween.from(".gear", { y: -280, rotation: 0.001, opacity: 0, duration: 6, ease: "
     .from(".base", { x: -130, rotation: 0.001, duration: 6, ease: "linear" }, '-=6')
     .from(".creator1", { x: -350, rotation: 0.001, duration: 6, ease: "linear" }, '-=6')
     .to(".text1", { y: -400, rotation: 0.001, zIndex: -1, opacity: 0, duration: 3, ease: "linear" }, '-=6')
-    .fromTo(".text2", {y: 400, rotation: 0.001}, {y: 0, rotation: 0.001, visibility: "visible", opacity: 1, duration: 6, ease: "linear" }, '-=6')
+    .from(".track", {y: 450, rotation: 0.001, opacity: 0, duration: 6, ease: "linear" }, '-=6')
     // .to(".text3",{ x: 0, duration: 6, ease: "linear" }, '-=6')
     // .to(".text4",{ x: 0, duration: 6, ease: "linear" }, '-=6')
     .to(".left", { display: "block" }, '-=4')
